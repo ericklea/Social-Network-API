d8a2ccfb8ce3dc33511c7fd9c2a323fd5e973976
L E <erickson.leah.j@gmail.com>
2023-06-25 17:55:05 -0600

    created user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,34 @@
 const { User, Application } = require('../models');
 
 module.exports = {
-    
+    async getAllUsers(req, res) {
+        try {
+            const users = await User.find({});
+            return res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getUserById(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.id })
+            .select('-__v')
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with this id!' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            return res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+};
