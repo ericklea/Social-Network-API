bb4fb42fb7ee45fccb5d309a9dbff0a95f201220
L E <erickson.leah.j@gmail.com>
2023-07-09 12:19:02 -0600

    re did user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,62 @@
 const User = require('../models/User');
 
-module.exports = {
-    async getAllUsers(req, res) {
-        try {
-            const users = await User.find({});
-            return res.json(users);
-        } catch (err) {
+const userController = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find()
+        .select('-__v')
+        .then(dbUserData => {
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
             res.status(500).json(err);
-        }
+        });
     },
-    async getUserById(req, res) {
-        try {
-            const user = await User.findOne({ _id: req.params.id })
-            .select('-__v')
 
-            if (!user) {
-                return res.status(404).json({ message: 'No user with this id!' });
+    // get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .select('-__v')
+        .populate('friends')
+        .populate('thoughts')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
             }
-
-            res.json(user);
-        } catch (err) {
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
             res.status(500).json(err);
-        }
+        });
     },
-    async createUser(req, res) {
-        try {
-            const user = await User.create(req.body);
-            return res.json(user);
-        } catch (err) {
-            res.status(500).json(err);
+
+    // create a user
+    createUser(req, res) {
+        User.create(req.body)
+        .then(dbUserData => {
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
         }
-    }
-};
+        );
+    },
+
+    // update a user
+    updateUserById(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId }, 
+            { $set: req.body }, 
+            { runValidators: true, new: true }
+        )
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: })
+            }
+        }
+        
+
