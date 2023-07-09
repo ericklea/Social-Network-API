d218861fa8356415c156e70c467552cdb72ecafd
L E <erickson.leah.j@gmail.com>
2023-07-09 12:40:56 -0600

    updated user routes and added comments

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,9 +3,19 @@ const {
     getAllUsers,
     getUserById,
     createUser,
+    updateUserById,
+    deleteUserById,
+    addFriend,
+    removeFriend,
 } = require('../../controllers/userController');
 
+// /api/users
 router.route('/').get(getAllUsers).post(createUser);
 
-router.route('/:userId').get(getUserById);
+// /api/users/userId
+router.route('/userId').get(getUserById).put(updateUserById).delete(deleteUserById);
+
+// /api/users/userId/friends/friendId
+router.route('/userId/friends/friendId').post(addFriend).delete(removeFriend);
+
 module.exports = router;
